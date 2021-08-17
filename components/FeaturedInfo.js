713b417990fbc1be6932812e33f15641f5ba46cf
bolg55 <bolger.kellen@gmail.com>
2021-08-16 23:11:01 -0400

    added more cards as well as currency conversion and tested data inputsfor profit view

--- components/FeaturedInfo.js
+++ components/FeaturedInfo.js
@@ -1,52 +1,60 @@
 import styles from "../styles/Featured.module.css";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 
+const featureData = [
+  {
+    id: 1,
+    title: "Recent Profit",
+    profit: 500,
+    icon: (
+      <ArrowDownward className={`${styles.featuredIcon} ${styles.downArrow}`} />
+    ),
+    iconTitle: "-6% ROI",
+    featuredSub: "Compared to last month",
+  },
+  {
+    id: 2,
+    title: "Year to Date Profit",
+    profit: 2021,
+    icon: <ArrowUpward className={`${styles.featuredIcon}`} />,
+    iconTitle: "+36% ROI",
+    featuredSub: "Compared to last year",
+  },
+  {
+    id: 3,
+    title: "All Time Profit",
+    profit: 19890,
+    icon: <ArrowUpward className={`${styles.featuredIcon}`} />,
+    iconTitle: "+86% ROI",
+    featuredSub: "Since Sign-up",
+  },
+];
+
 const FeaturedInfo = () => {
+  let formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+
   return (
     <div className={styles.featured}>
-      <div className={styles.featuredItem}>
-        <span className={styles.featuredTitle}>Recent Profit</span>
-        <div className={styles.featuredProfitContainer}>
-          <span className={styles.featuredProfit}>$420</span>
-          <span className={styles.featuredROI}>
-            {" "}
-            <ArrowDownward
-              className={`${styles.featuredIcon} ${styles.downArrow}`}
-            />
-            -6% ROI
-          </span>
+      {featureData.map((data) => (
+        <div key={data.id} className={styles.featuredItem}>
+          <span className={styles.featuredTitle}>{data.title}</span>
+          <div className={styles.featuredProfitContainer}>
+            <span className={styles.featuredProfit}>
+              {formatter.format(`${data.profit}`)}
+            </span>
+            <span className={styles.featuredROI}>
+              {data.icon}
+              {data.iconTitle}
+            </span>
+          </div>
+          <span className={styles.featuredSub}>{data.featuredSub}</span>
         </div>
-        <span className={styles.featuredSub}> Compared to last month</span>
-      </div>
-      <div className={styles.featuredItem}>
-        <span className={styles.featuredTitle}>Year to Date Profit</span>
-        <div className={styles.featuredProfitContainer}>
-          <span className={styles.featuredProfit}>$2,021</span>
-          <span className={styles.featuredROI}>
-            {" "}
-            <ArrowUpward
-              className={`${styles.featuredIcon} ${styles.upArrow}`}
-            />
-            +36% ROI
-          </span>
-        </div>
-        <span className={styles.featuredSub}> Compared to last year</span>
-      </div>
-
-      <div className={styles.featuredItem}>
-        <span className={styles.featuredTitle}>All Time Profit</span>
-        <div className={styles.featuredProfitContainer}>
-          <span className={styles.featuredProfit}>$19,890</span>
-          <span className={styles.featuredROI}>
-            {" "}
-            <ArrowUpward
-              className={`${styles.featuredIcon} ${styles.upArrow}`}
-            />
-            +69% ROI
-          </span>
-        </div>
-        <span className={styles.featuredSub}> Since Signup</span>
-      </div>
+      ))}
     </div>
   );
 };
